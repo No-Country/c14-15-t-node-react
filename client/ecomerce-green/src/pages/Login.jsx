13d2be43557d5b7071e0f67278da319ae3159bd7
Lourdes <lulyavalosmartinez@gmail.com>
2023-10-13 20:29:31 -0300

    proyecto completo

--- client/ecomerce-green/src/pages/Login.jsx
+++ client/ecomerce-green/src/pages/Login.jsx
@@ -24,9 +24,9 @@ export const Login = () => {
     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <form className="space-y-6" action="#" method="POST">
         <div>
-          <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+          {/* <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
             Email
-          </label>
+          </label> */}
           <div className="">
             <input
               id="email"
@@ -34,16 +34,17 @@ export const Login = () => {
               type="email"
               autoComplete="email"
               required
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              className="block w-full  border-0 py-1.5 text-gray-900 shadow-sm  ring-inset  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder='Email'
             />
           </div>
         </div>
 
         <div>
           <div className="flex items-center justify-between">
-            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-              Contraseña
-            </label>
+             <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+             
+            </label> 
             
           </div>
           <div className="mt-2">
@@ -53,30 +54,31 @@ export const Login = () => {
               type="password"
               autoComplete="current-password"
               required
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              className="block w-full  border-0 py-1.5 text-gray-900 shadow-sm  ring-inset ring-gray-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+             placeholder='Contraseña' 
             />
           </div>
         </div>
-        <div className="text-sm">
-              <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+        <div className="text-sm flex justify-center">
+              <a href="#" className="font-semibold text-white-600 hover:text-indigo-500">
                 ¿Has olvidado tu contraseña?
               </a>
             </div> 
 
-        <div>
+        <div className='flex justify-center '>
           <button
             type="submit"
-            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="flex w-[160px] h-[40px] justify-center rounded-md bg-orange-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
           >
-           Ingresar
+          <p className='text-2x5'>Ingresar</p>  
           </button>
         </div>
       </form>
 
       <p className="mt-10 text-center text-sm text-gray-500">
         ¿No estas registrado?{' '}
-        <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-          Crear cuenta
+        <a href="#" className="font-semibold leading-6 text-orange-600 hover:text-orange-500">
+        Crear cuenta
         </a>
       </p>
     
