9bec407028836ee6923dca4c694053984e016146
CarlosV319 <93988408+CarlosV319@users.noreply.github.com>
2023-10-16 15:56:44 -0300

    create store.js, authSlice.js and provider store

--- client/ecomerce-green/src/main.jsx
+++ client/ecomerce-green/src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Provider } from "react-redux";
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { store } from "./redux/store.js";
 import './index.css'
 // import './style/Card.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
 )
