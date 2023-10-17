f448edba4573be2e60106e887abb45ab3eb9fd3c
YsisC <ysisclongart@gmail.com>
2023-10-17 17:14:06 -0300

    change provider

--- client/ecomerce-green/src/App.jsx
+++ client/ecomerce-green/src/App.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import { Provider } from "react-redux";
 import Home from "./pages/Home";
 import { Login } from './pages/Login';
 import Cart from './pages/Cart';
 import Product from './pages/Product';
 import Products from './pages/Products';
 import DynamicTitlePage from './pages/DynamicTitlePage';
-import { store } from './redux/store';
+
 
 const App = () => {
   return (
-    <Provider store={store}>
-
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
@@ -23,7 +20,7 @@ const App = () => {
         <Route exact path="/dinamic" element={<DynamicTitlePage />} />
       </Routes>
     </Router>
-    </Provider>
+
   )
 }
 
--- client/ecomerce-green/src/redux/store/product/productSlice.js
+++ client/ecomerce-green/src/redux/store/product/productSlice.js
@@ -58,7 +58,18 @@ const productSlice = createSlice({
         'INCREMENT'
       );
       state.cart = modifiedCart;
-    }
+    },
+    decrementProduct: (state, action) => {
+      const modifiedCart = modifyQtyByOne(
+        state.cart,
+        action.payload,
+        'DECREMENT'
+      );
+      state.cart = modifiedCart;
+    },
+    resetCart: (state) => {
+      state.cart = [];
+    },
   },
   extraReducers: (builder) => {
     builder
@@ -85,6 +96,6 @@ export const fetchProducts = createAsyncThunk("product", async () => {
   const data = await axios.get(`${base_url}products`).then((res) => res.data);
   return data;
 });
-export const { incrementProduct  } =
+export const { incrementProduct, decrementProduct, resetCart  } =
   productSlice.actions;
 export default productSlice.reducer;
