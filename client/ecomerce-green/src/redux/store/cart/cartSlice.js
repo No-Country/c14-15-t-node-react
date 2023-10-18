a377bdfcc52531021ae6e0946ef397dee4f880dc
YsisC <ysisclongart@gmail.com>
2023-10-18 15:23:51 -0300

    auth slice

--- client/ecomerce-green/src/redux/store/cart/cartSlice.js
+++ client/ecomerce-green/src/redux/store/cart/cartSlice.js
@@ -1,53 +1,46 @@
-import {  createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
+  cart: [],
+};
 
-    cart: [],
-  };
-  
-  const modifyQtyByOne = (
-    cart,
-    selectedProduct,
-    modificationType
-  ) => {
-    const previousCart = [...cart];
-  
-    const productInCart = previousCart.find(
-      (product) => product.id === selectedProduct.id
-    );
-  
-    let newCart = [];
-  
-    if (!productInCart) {
-      previousCart.push({ ...selectedProduct, quantity: 1 });
-      newCart = previousCart;
+const modifyQtyByOne = (cart, selectedProduct, modificationType) => {
+  const previousCart = [...cart];
+
+  const productInCart = previousCart.find(
+    (product) => product.id === selectedProduct.id
+  );
+
+  let newCart = [];
+
+  if (!productInCart) {
+    previousCart.push({ ...selectedProduct, quantity: 1 });
+    newCart = previousCart;
+  } else {
+    const filteredCart = previousCart.filter((p) => p.id !== productInCart.id);
+
+    const modification = modificationType === "INCREMENT" ? 1 : -1;
+
+    productInCart.quantity = productInCart.quantity + modification;
+
+    if (productInCart.quantity === 0) {
+      newCart = [...filteredCart];
     } else {
-      const filteredCart = previousCart.filter(
-        (p) => p.id !== productInCart.id
-      );
-  
-      const modification = modificationType === 'INCREMENT' ? 1 : -1;
-  
-      productInCart.quantity = productInCart.quantity + modification;
-  
-      if (productInCart.quantity === 0) {
-        newCart = [...filteredCart];
-      } else {
-        newCart = [...filteredCart, productInCart];
-      }
+      newCart = [...filteredCart, productInCart];
     }
-    return newCart;
-  };
+  }
+  return newCart;
+};
 
-  const cartSlice =createSlice({
-    name: "cart",
+const cartSlice = createSlice({
+  name: "cart",
   initialState,
   reducers: {
     incrementProduct: (state, action) => {
       const modifiedCart = modifyQtyByOne(
         state.cart,
         action.payload,
-        'INCREMENT'
+        "INCREMENT"
       );
       state.cart = modifiedCart;
       localStorage.setItem("cart", JSON.stringify(state.cart));
@@ -56,7 +49,7 @@ const initialState = {
       const modifiedCart = modifyQtyByOne(
         state.cart,
         action.payload,
-        'DECREMENT'
+        "DECREMENT"
       );
       state.cart = modifiedCart;
       localStorage.setItem("cart", JSON.stringify(state.cart));
@@ -66,10 +59,9 @@ const initialState = {
       localStorage.setItem("cart", JSON.stringify(state.cart));
     },
   },
-  })
+});
 
-  export const { incrementProduct, decrementProduct, resetCart  } =
+export const { incrementProduct, decrementProduct, resetCart } =
   cartSlice.actions;
 
-  export default cartSlice.reducer;
-
+export default cartSlice.reducer;
