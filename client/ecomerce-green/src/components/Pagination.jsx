f226eb6473aba4c900ba4c6b4a74ceb51afd9301
YsisC <ysisclongart@gmail.com>
2023-10-24 18:31:15 -0300

    pagination e integracion de la api

--- client/ecomerce-green/src/components/Pagination.jsx
+++ client/ecomerce-green/src/components/Pagination.jsx
@@ -1,48 +1,48 @@
 import React from 'react'
 
-const Pagination = () => {
-  return (
+const Pagination = ({page,  totalPages, onPageChange }) => {
+    const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
+  console.log(pages)
+    return (
     <div className="container mx-auto px-4">
     <nav className="flex flex-row flex-nowrap justify-between md:justify-center items-center" aria-label="Pagination">
-        {/* <!-- Previous Page Button --> */}
-        <a className="flex w-10 h-10 mr-1 justify-center items-center rounded-full border border-gray-200 bg-white dark:bg-gray-800 text-black dark:text-white hover:border-gray-300 dark:hover:border-gray-600"
-            href="#" title="Previous Page">
-            <span className="sr-only">Previous Page</span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                stroke="currentColor" className="block w-5 h-5">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-            </svg>
-        </a>
+    {/* Previous Page Button */}
+    <button
+          className="flex w-10 h-10 mr-1 justify-center items-center rounded-full border border-gray-200 bg-white dark:bg-gray-800 text-black dark:text-white hover:border-gray-300 dark:hover:border-gray-600"
+          onClick={() => onPageChange(page - 1)}
+          disabled={page === 1}
+          title="Previous Page"
+        >
+          <span className="sr-only">Previous Page</span>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="block w-5 h-5">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+          </svg>
+        </button>
         {/* <!-- Page Buttons (1 to 5) --> */}
-        <a className="hidden md:flex w-10 h-10 mx-1 justify-center items-center rounded-full border border-gray-200 bg-white dark:bg-gray-700 text-black dark:text-white hover:border-gray-300 dark:hover:border-gray-600"
-            href="#" title="Page 1">
-            1
-        </a>
-        <a className="hidden md:flex w-10 h-10 mx-1 justify-center items-center rounded-full border border-gray-200 bg-white dark:bg-gray-700 text-black dark:text-white hover:border-gray-300 dark:hover:border-gray-600"
-            href="#" title="Page 2">
-            2
-        </a>
-        <a className="hidden md:flex w-10 h-10 mx-1 justify-center items-center rounded-full border border-black dark:border-white dark:bg-black dark:text-white pointer-events-none"
-            href="#" aria-current="page" title="Page 3">
-            3
-        </a>
-        <a className="hidden md:flex w-10 h-10 mx-1 justify-center items-center rounded-full border border-gray-200 bg-white dark:bg-gray-700 text-black dark:text-white hover:border-gray-300 dark:hover:border-gray-600"
-            href="#" title="Page 4">
-            4
-        </a>
-        <a className="hidden md:flex w-10 h-10 mx-1 justify-center items-center rounded-full border border-gray-200 bg-white dark:bg-gray-700 text-black dark:text-white hover:border-gray-300 dark:hover:border-gray-600"
-            href="#" title="Page 5">
-            5
-        </a>
-  
-        <a className="flex w-10 h-10 ml-1 justify-center items-center rounded-full border border-gray-200 bg-white dark:bg-gray-800 text-black dark:text-white hover:border-gray-300 dark:hover:border-gray-600"
-            href="#" title="Next Page">
-            <span className="sr-only">Next Page</span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                stroke="currentColor" className="block w-5 h-5">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-            </svg>
-        </a>
+
+        {pages.map((pageNumber) => (
+          <button
+            key={pageNumber}
+            className={`w-10 h-10 mx-1 justify-center items-center rounded-full border ${pageNumber === page ? 'border-black dark:border-white dark:bg-black dark:text-white' : 'border-gray-200 bg-white dark:bg-gray-700 text-black dark:text-white hover:border-gray-300 dark:hover:border-gray-600'}`}
+            onClick={() => onPageChange(pageNumber)}
+            title={`Page ${pageNumber}`}
+          >
+            {pageNumber}
+          </button>
+        ))}
+    
+      {/* Next Page Button */}
+      <button
+      className="flex w-10 h-10 ml-1 justify-center items-center rounded-full border border-gray-200 bg-white dark:bg-gray-800 text-black dark:text-white hover:border-gray-300 dark:hover:border-gray-600"
+      onClick={() => onPageChange(page + 1)}
+      disabled={page === totalPages}
+      title="Next Page"
+    >
+      <span className="sr-only">Next Page</span>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="block w-5 h-5">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+      </svg>
+    </button>
     </nav>
 </div>
   )
