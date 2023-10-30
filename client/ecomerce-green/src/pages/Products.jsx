6e8e57b854367499e077a2ddde862dababbc128d
CarlosV319 <carlos.alberto_88@hotmail.com>
2023-10-29 21:12:39 -0300

    .

--- client/ecomerce-green/src/pages/Products.jsx
+++ client/ecomerce-green/src/pages/Products.jsx
@@ -33,9 +33,8 @@ const Products = () => {
   }, [products.products]);
 
   useEffect(() => {
-    if(productsfilter.products.length >0){
-
-      setProductsData(productsfilter.products); // Actualizar el estado cuando cambian los productos
+    if (productsfilter.products && productsfilter.products.length > 0) {
+      setProductsData(productsfilter.products);
     }
   }, [productsfilter.products]);
 
