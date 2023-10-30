d3a851a48f371f8019e4121d00055964c181ff02
YsisC <ysisclongart@gmail.com>
2023-10-30 14:15:17 -0300

    router checkout

--- client/ecomerce-green/src/PrivateRoute/PrivateRoute.jsx
+++ client/ecomerce-green/src/PrivateRoute/PrivateRoute.jsx
@@ -4,9 +4,9 @@ import { useSelector} from "react-redux"
 const PrivateRoute = () => {
 
 
-  const { user, token } = useSelector((state) => state.authv)
-  console.log(user)
-if ( user & token) {
+  const {  userToken } = useSelector((state) => state.authv)
+
+if (  userToken) {
   
   return  <Outlet /> 
 }
