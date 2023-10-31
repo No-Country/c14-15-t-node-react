a375d259f52702e97dc1a7ec3090d78a673098f5
Mary Laura Carolina Limay Oliva <limayolivamary@gmail.com>
2023-10-31 16:48:51 -0500

    Ultimos cambios a PageNotFound v 2.0

--- client/ecomerce-green/src/pages/ProductDetail.jsx
+++ client/ecomerce-green/src/pages/ProductDetail.jsx
@@ -5,22 +5,40 @@ import { useParams } from "react-router-dom";
 import HeroStore from "../components/HeroStore";
 import { useDispatch, useSelector } from "react-redux";
 import { getProduct } from "../redux/store/productid/productAction";
+import { useNavigate } from 'react-router-dom';
 
 const Product = () => {
+  const history=useNavigate();
   const dispatch = useDispatch();
   const { product } = useSelector((state) => state.product);
   const {id}=useParams();
 
   useEffect(() => {
     dispatch(getProduct(id)); 
+    
   }, [dispatch, id]);
+  console.log('este es la data',product);
+  const isEmptyObject = (obj) => {
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-  return (
+
+  const goTo404=()=>{
+    history('/*');
+  }
+   return (
     <MainLayout>
     <HeroStore />
-      <main>
+      {isEmptyObject(product) ? (
+          goTo404()):<main>
         <ProductSelected product={product} id={id}/> 
-      </main>
+      </main> }
+      
     </MainLayout>
   );
 };
